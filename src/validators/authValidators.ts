0db5d36ca874c21a37bfa1618b033cb42fd5dca4
LL-Etiane <lukongleinyuyetiane@gmail.com>
2025-03-08 23:50:53 +0100

    fix email sending and refactor code adding validation

---
+++ src/validators/authValidators.ts
@@ -0,0 +1,17 @@
+import { check } from "express-validator";
+import { RolesEnum } from "../enums";
+
+export const validateLoginRequest = [
+  check("email", "Please enter a valid email address").isEmail(),
+  check("password", "Password is required").exists(),
+];
+
+export const validateRegisterRequest = [
+  check("first_name", "First name is required").exists(),
+  check("last_name", "Last name is required").exists(),
+  check("email", "Please enter a valid email address").isEmail(),
+  check("password", "Password is required").exists(),
+    check("role")
+        .isIn([RolesEnum.STUDENT, RolesEnum.TUTOR])
+        .withMessage("Invalid role provided, Role can either be student or tutor"),
+];
